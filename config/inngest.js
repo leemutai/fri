384d0c2a9245e115eebe11d34550b1c80ea19eb6
leemutai <mutailee361@gmail.com>
2025-03-07 01:46:21 +0300

    ✅ Integrated Inngest for event-driven user sync from Clerk
        Added syncUserCreation function to store new user data in MongoDB when a Clerk user is created.

---
+++ config/inngest.js
@@ -0,0 +1,29 @@
+import { Inngest } from "inngest";
+import { connect } from "mongoose";
+import connectDB from "./db";
+import User from "@/models/User";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "quickcart-next" });
+
+
+// Ingest Fuction to save a user data to a  database
+
+export const syncUserCreation  =  inngest.createFunction(
+    {
+        id:"sync-user-from-clerk",
+    },
+    {event:"clerk/user.created"},
+    async (event)=>{
+        const {id,first_name, last_name,email_addresses, image_url} = event.data
+        const userData = {
+            _id:id,
+            email: email_addresses[0].email_address,
+            name:first_name + ' ' + last_name,
+            imageUrl:image_url
+
+        }
+        await connectDB()
+        await User.create(userData)
+    }
+)
