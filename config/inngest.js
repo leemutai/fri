3890ccd6f6d32e02bcaa307637b9590a0cd5c286
leemutai <mutailee361@gmail.com>
2025-03-10 01:07:50 +0300

    Added syncUserUpdation Function for Updating User Data, Added Inngest function for syncing updated user data from Clerk

--- config/inngest.js
+++ config/inngest.js
@@ -27,3 +27,37 @@ export const syncUserCreation  =  inngest.createFunction(
         await User.create(userData)
     }
 )
+
+// Ingest Function to update user data in the database
+export const syncUserUpdation = inngest.createFunction(
+
+    {
+        id:'update-user-from-clerk'
+    },
+    {event:"clerk/user.updated"},
+    async ({event})=>{
+        const {id,first_name, last_name,email_addresses, image_url} = event.data
+        const userData = {
+            _id:id,
+            email: email_addresses[0].email_address,
+            name:first_name + ' ' + last_name,
+            imageUrl:image_url
+        }
+        await connectDB()
+        await User.findByIdAndUpdate(id,userData)
+
+    }
+)
+
+// Ingest Function to delete user data from the database
+export const syncUserDeletion = inngest.createFunction(
+    {
+        id:'delete-user-with-clerk'
+    },
+    {event:"clerk/user.deleted"},
+    async ({event})=>{
+        const {id} = event.data
+        await connectDB()
+        await User.findByIdAndDelete(id)
+    }
+)
